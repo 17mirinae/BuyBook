f745fb7a3839b536432d020727ad1c6a9ca44f83
MinGW <mirinae@skuniv.ac.kr>
2021-09-11 00:39:21 +0900

    Controller Update

--- src/main/java/com/graduate/Controller/UserController.java
+++ src/main/java/com/graduate/Controller/UserController.java
@@ -7,5 +7,21 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequestMapping(value = "/user/*")
 public class UserController {
-	
+	// 회원가입
+	@RequestMapping(value = "/userSignUp", method = RequestMethod.GET)
+	public String userSignUp() {
+		return "userSignUp";
+	}
+
+	// 회원 수정
+	@RequestMapping(value = "/userUpdate", method = RequestMethod.GET)
+	public String userUpdate() {
+		return "userUpdate";
+	}
+
+	// 로그인
+	@RequestMapping(value = "/userSignIn", method = RequestMethod.GET)
+	public String userSignIn() {
+		return "userSignIn";
+	}
 }
